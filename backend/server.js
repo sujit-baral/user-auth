ff2588af654517a50e81c4929d4520decb8df66e
Sujit <sujit124baral@gmail.com>
2025-04-15 07:17:16 +0530

    Adding user authentication code

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,17 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 dotenv.config();
 
 const app = express();
