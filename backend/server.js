8521f1751e350b90d4365e4e9407a5343c8e2e1d
Sujit <sujit124baral@gmail.com>
2025-04-15 23:55:29 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,11 @@ const express = require("express");
 const connectDB = require('./config/db');
 const cors = require("cors");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/authRoutes");
-
-const path = require('path');
-
-const app = express();
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+const authRoutes = require("./routes/authRoutes"); 
 
 dotenv.config();
 
-
+const app = express();
 
 // Middleware
 app.use(cors());
