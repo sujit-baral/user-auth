494dc3e9e8b7f5810c62d8123e01e63d53c918de
Sujit <sujit124baral@gmail.com>
2025-04-17 16:26:07 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,21 @@ const connectDB = require('./config/db');
 const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes"); 
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://your-frontend-domain.netlify.app', // will update this after frontend deploy
+  credentials: true
+}));
+
+
+
+
 app.use(express.json());
 
 // Routes
@@ -18,8 +26,8 @@ app.use("/api", authRoutes);
 
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 
